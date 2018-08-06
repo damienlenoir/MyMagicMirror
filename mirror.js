ac9b2fc14f84b986b6e58ad337321a890697ca39
damien <dlenoir@pageup.fr>
2018-08-06 11:50:34 +0200

    add forecast

--- mirror.js
+++ mirror.js
@@ -18,12 +18,12 @@ function displayTime() {
     document.getElementById("time").innerHTML= getTime();
 }
 
-setInterval(function(){ displayTime(); }, 1000);
+// setInterval(function(){ displayTime(); }, 1000);
 
 /* MODULE METEO */
 
 function callMeteo() {
-    const url = 'http://api.openweathermap.org/data/2.5/forecast?APPID=c3581901e61477aaa07f4410fe9868c3&id=3021372&lang=fr&units=metric';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?APPID=c3581901e61477aaa07f4410fe9868c3&id=3021372&lang=fr&units=metric';
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -35,11 +35,29 @@ function callMeteo() {
     xhttp.send();
 }
 
-function setMeteoValue(res) {
-    let actualMeteo = res.list[0];
-    console.log(actualMeteo);
+function callMeteoForecast() {
+    const url = 'http://api.openweathermap.org/data/2.5/forecast?APPID=c3581901e61477aaa07f4410fe9868c3&id=3021372&lang=fr&units=metric';
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            setMeteoForecast( JSON.parse(this.responseText) );
+            //console.log( this.responseText );
+        }
+    };
+    xhttp.open( "GET", url, true);
+    xhttp.send();
+}
 
-    
+function setMeteoValue(res) {
+    console.log(res);
+    let vent = convertWindSpeed(res.wind.speed);
+}
+
+function setMeteoForecast(res) {
+}
+
+function convertWindSpeed( speed ) {
+    return speed *3.6;
 }
 
 /*  MODULE POST-IT
@@ -51,4 +69,8 @@ function setMeteoValue(res) {
 
 /* MODULE INDICE VELO
 
+ */
+
+/*
+TODO: un Setter general qui prend en parma le nom qui correspond à l'id dans l'HTML
  */
