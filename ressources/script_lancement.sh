ba0dac9935b434148a58f9c2c3b797ea99dffa29
damien <dlenoir@pageup.fr>
2018-09-29 19:22:56 +0200

    updating stuff

--- ressources/script_lancement.sh
+++ ressources/script_lancement.sh
@@ -1,3 +1,3 @@
-pm2 start MyMagicMirror/app.js --max-memory-restart 100M
-chromium-browser MyMagicMirror/mirror.html --start-fullscreen
+pm2 start MyMagicMirror/app.js --max-memory-restart 100M 
+chromium-browser MyMagicMirror/mirror.html --start-fullscreen -disable--infobars
 
