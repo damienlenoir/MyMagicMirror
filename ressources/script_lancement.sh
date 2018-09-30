339500b6c25d137193c1234f458b0eb9c9a27ac9
damien <dlenoir@pageup.fr>
2018-09-30 18:35:40 +0200

    add working python script

--- ressources/script_lancement.sh
+++ ressources/script_lancement.sh
@@ -1,3 +1,3 @@
-pm2 start MyMagicMirror/app.js --max-memory-restart 100M 
-chromium-browser MyMagicMirror/mirror.html --start-fullscreen -disable--infobars
+pm2 start /home/pi/MyMagicMirror/app.js --max-memory-restart 100M 
+chromium-browser /home/pi/MyMagicMirror/mirror.html --start-fullscreen -disable--infobars
 
